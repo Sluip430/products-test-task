bca0c9f7306205e9f1b936cdd176402555d70971
Sluip430 <82076886+Sluip430@users.noreply.github.com>
2024-05-24 14:39:30 +0300

    init project

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+  
+  const config = new DocumentBuilder()
+    .setTitle('PizzaWay')
+    .setDescription('PizzaWay docs')
+    .setVersion('1.0')
+    .addTag('PizzaWay')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  const port = process.env.PORT;
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    exposedHeaders: 'Set-Cookie',
+  });
+  await app.listen(port, () => console.log(`Server started on ${port}`));
+}
+bootstrap();
